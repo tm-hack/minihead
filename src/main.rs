ffda1f77402872260ed39ebf2646fcf59cc6f0d7
tm-hack <takufrom1124@gmail.com>
2023-02-01 23:52:27 +0900

    dispay all message

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::{env, fs::File, io::Read};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let filename = &args[1];
+
+    let mut f = File::open(filename).expect("file not found");
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("something went wrong readin the file");
+
+    println!("With text:\n{}", contents);
 }
